7262fac1001a98bdf76f866193da3b700b25d91e
Saviour1001 <harshghodkar@gmail.com>
2022-09-02 17:30:15 +0530

    updates

--- src/rpc/getExtendedPublicKey.ts
+++ src/rpc/getExtendedPublicKey.ts
@@ -1,9 +1,10 @@
 // import * as bip32 from "bip32";
-// import { Network, networks } from "bitcoinjs-lib";
-// import { BIP32Interface } from "bip32";
+import { Network, networks } from "bitcoinjs-lib";
+import { BIP32Interface } from "bip32";
 import { Wallet, ScriptType, BIP44CoinTypeNode } from "../interface";
+import { getBIP44AddressKeyDeriver } from "@metamask/key-tree";
 
-export async function getExtendedPublicKey(wallet: Wallet): Promise<string> {
+export async function getExtendedPublicKey(wallet: Wallet): Promise<Buffer> {
   const result = await wallet.request({
     method: "snap_confirm",
     params: [
@@ -19,8 +20,56 @@ export async function getExtendedPublicKey(wallet: Wallet): Promise<string> {
     // let accountNode = await extractAccoutPrivateKey(wallet, network);
     // let accountPublicKey = accountNode.neutered();
     // return accountPublicKey.toBase58();
-    return "Hello";
+    // console.log("redd");
+    let response = await extractAccoutPrivateKey(wallet);
+    return response;
   } else {
     throw new Error("User reject to access the key");
   }
 }
+
+export async function extractAccoutPrivateKey(wallet: Wallet): Promise<Buffer> {
+  let coinType: number = 501;
+  // if (network != networks.bitcoin) {
+  //   coinType = 1;
+  // }
+
+  const methodName = `snap_getBip44Entropy_${coinType}`;
+  const bitcoin44node = (await wallet.request({
+    method: methodName,
+  })) as BIP44CoinTypeNode;
+  const privateKeyBuffer = Buffer.from(bitcoin44node.privateKey, "hex");
+  const chainCodeBuffer = Buffer.from(bitcoin44node.chainCode, "hex");
+  console.log(privateKeyBuffer);
+  console.log(chainCodeBuffer);
+  return chainCodeBuffer;
+  // let node: BIP32Interface = bip32.fromPrivateKey(
+  //   privateKeyBuffer,
+  //   chainCodeBuffer,
+  //   network
+  // );
+  //@ts-ignore
+  // ignore checking since no function to set depth for node
+  // node.__DEPTH = 2;
+  // //@ts-ignore
+  // // ignore checking since no function to set index for node
+  // node.__INDEX = HIGHEST_BIT + 0;
+  // return node.deriveHardened(0);
+}
+// export async function extractAccoutPrivateKey(
+//   wallet: Wallet,
+//   network: string
+// ): Promise<string> {
+//   const result = await wallet.request({
+//     method: "snap_confirm",
+//     params: [
+//       {
+//         prompt: "Access your account private key?",
+//         description:
+//           "Do you want to allow this app to access your account private key?",
+//       },
+//     ],
+//   });
+
+//   return "Hello";
+// }
